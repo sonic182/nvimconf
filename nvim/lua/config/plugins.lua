c438b4667a8be3f8f7d150e071342f37e6369ca3
Johanderson <johander1822@gmail.com>
2026-04-10 16:31:53 +0200

    telescope update

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -25,10 +25,27 @@ return {
     build = function() vim.fn["fzf#install"]() end,
     dependencies = { "junegunn/fzf.vim" }
   },
-  { "folke/which-key.nvim",          config = function() require("which-key").setup {} end },
+  { "folke/which-key.nvim",            config = function() require("which-key").setup {} end },
 
   -- AI & Completions
-  { "nvim-telescope/telescope.nvim", tag = "0.1.8",                                        dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "nvim-telescope/telescope.nvim",
+    -- tag = "0.2.1",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- optional but recommended
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          preview = {
+            treesitter = false,
+          },
+        },
+      })
+    end,
+  },
   {
     "olimorris/codecompanion.nvim",
     config = function() require("config.codecompanion") end,
@@ -85,29 +102,29 @@ return {
     end,
   },
   { "terryma/vim-multiple-cursors" },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      -- bigfile = { enabled = true },
-      -- dashboard = { enabled = true },
-      -- explorer = { enabled = true },
-      indent = { enabled = true },
-      -- input = { enabled = true },
-      picker = { enabled = true },
-      -- notifier = { enabled = true },
-      -- quickfile = { enabled = true },
-      -- scope = { enabled = true },
-      -- scroll = { enabled = true },
-      -- statuscolumn = { enabled = true },
-      -- words = { enabled = true },
-    },
-  },
+  -- {
+  --   "folke/snacks.nvim",
+  --   priority = 1000,
+  --   lazy = false,
+  --   ---@type snacks.Config
+  --   opts = {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --     -- bigfile = { enabled = true },
+  --     -- dashboard = { enabled = true },
+  --     -- explorer = { enabled = true },
+  --     indent = { enabled = true },
+  --     -- input = { enabled = true },
+  --     picker = { enabled = true },
+  --     -- notifier = { enabled = true },
+  --     -- quickfile = { enabled = true },
+  --     -- scope = { enabled = true },
+  --     -- scroll = { enabled = true },
+  --     -- statuscolumn = { enabled = true },
+  --     -- words = { enabled = true },
+  --   },
+  -- },
   {
     "janko-m/vim-test",
     config = function()
